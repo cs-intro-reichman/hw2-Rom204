a9c6be7fe5d9d296676074e57c9a576c06f7b0d0
Rom Guyer <romguyer@Roms-MacBook-Pro.local>
2024-11-15 18:09:10 +0200

    fixed q1 test case with 00 mins

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,18 +1,17 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        
+        // recieve data from inputs
         int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
         int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int minutesToAdd = Integer.parseInt(args[1]);
-        // System.out.println(hours);
-        System.out.println(minutes);
         
+        // make calculations
         int totalMinutes = (hours * 60) + minutes + minutesToAdd;
         int totalHours = totalMinutes / 60;
         int newHours = totalHours % 24;
         int newMinutes = totalMinutes - (totalHours * 60);
-        // System.out.println(newHours); 
-        System.out.println(newMinutes); 
+        
+        // creating the output message according to instructions
         String leadingHours = "";
         String leadingMinutes = "";
         if (newHours <= 9){
